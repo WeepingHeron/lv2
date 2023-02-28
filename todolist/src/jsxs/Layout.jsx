1081e046671fc5b5cc75ef678c8cb99e02e2c3e9
WeepingHeron <kimtaehoon_98@naver.com>
2023-02-28 16:56:54 +0900

    설명 추가

--- todolist/src/jsxs/Layout.jsx
+++ todolist/src/jsxs/Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './csss/layout.css';
 
 const Layout = (props) => {
-    return <div className='layout'>{props.children}</div>;
+    return <div className='layout'>{props.children}</div>;          //layout div 태그 사이에 props를 넣어 layout 사이 내용을 props로 쓸 수 있게 한다.
 };
 
 export default Layout;
