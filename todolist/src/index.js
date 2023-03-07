6974611f08a4f68f8c8658f4b91c80bc8bf124fd
WeepingHeron <kimtaehoon_98@naver.com>
2023-03-07 17:21:17 +0900

    메인 페이지

--- todolist/src/index.js
+++ todolist/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './jsxs/csss/index.css';
+import './index.css';
 import App from './jsxs/App';
 import reportWebVitals from './reportWebVitals';
 
